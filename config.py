9176d61eb4cd3407ab54473834da7932ea250d23
arunvemana <arunsai4b6@gmail.com>
2025-04-27 04:44:25 +0530

    Added creditionals

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+
+# Environment variables
+os.environ['ENV_USR'] = ''
+os.environ['ENV_PASS'] = ''
+
+USERNAME = os.getenv('ENV_USR')
+PASSWORD = os.getenv('ENV_PASS')
+
+if not USERNAME and PASSWORD:
+    raise  EnvironmentError(
+        f"Missing credtionals , OF BOTH ENV_USR AND ENV_USR: {USERNAME} AND {PASSWORD}"
+    )
